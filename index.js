e7de5f0ce7010cbb4aa13d5e51c99f24376b9709
Faiqa <faiqa28bpl@gmail.com>
2026-08-15 00:41:26 +0530

    package installation and imports

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from  'figlet';
+import { createSpinner } from 'nanospinner';
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    "Cool people never give up!! \n"
+  );
+}
